f36e03963e81f77ab267e3e0e151ce68f5740743
Jean Paul <jeanpaulwebb@gmail.com>
2023-08-24 02:34:40 -0300

    :pencil2: Merchant

--- src/main/java/com/br/backend/picpay/services/UserService.java
+++ src/main/java/com/br/backend/picpay/services/UserService.java
@@ -1,5 +1,6 @@
 package com.br.backend.picpay.services;
 
+import com.br.backend.picpay.annotations.Info;
 import com.br.backend.picpay.domain.user.User;
 import com.br.backend.picpay.domain.user.UserType;
 import com.br.backend.picpay.repositories.UserRepository;
@@ -7,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-
+@Info(author = "Jean Paul | jeanpaulwebb@gmail.com", date = "24/08/2023")
 @Service
 public class UserService {
 
@@ -21,7 +22,7 @@ public class UserService {
      */
     public void validationTransaction(User payer, BigDecimal amount) throws Exception {
         if (payer.getUserType() == UserType.MERCHANT) {
-            throw new Exception("Usuário do tipo logista não está autorizado a realizar transação!");
+            throw new Exception("Usuário do tipo comerciante não está autorizado a realizar transação!");
         }
         if ((payer.getBalance().compareTo(amount)) < 0) {
             throw new Exception("Usuário não tem saldo suficiente!");
