536314a9ef9750bc2551bce85b659c24685c29e4
Jean Paul <jeanpaulwebb@gmail.com>
2023-08-24 00:48:06 -0300

    :tada: Creating initial spring framework project

---
+++ src/main/java/com/br/backend/picapay/BackendVacancyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.br.backend.picapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVacancyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVacancyChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/backend/picapay/BackendVacancyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.backend.picapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVacancyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
